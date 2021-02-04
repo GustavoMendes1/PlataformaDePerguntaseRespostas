b5c55a5c3c2c459e6bdb5237df57716ed7003909
Gustavo Mendes <gustavooliveira@great.ufc.br>
2021-02-04 04:27:00 -0300

    Adicionando endpoint de pergunta/id

--- index.js
+++ index.js
@@ -44,6 +44,21 @@ app.post("/salvarpergunta",(req,res)=>{
     });
 });
 
+app.get("/pergunta/:id",(req,res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id:id}
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+            res.render("pergunta",{
+                pergunta:pergunta
+            });
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
 
 app.listen(8081,() => {
 
